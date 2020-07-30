f42dce27f6bf194765098cfddbbefb004aaaccb1
Sven Krumbeck <sven.krumbeck@gmail.com>
2020-07-30 14:57:03 +0200

    First Commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+  public function show($slug)
+  {
+    //$post = \DB::table('posts')->where('slug', $slug)->first();
+    $post = Post::where('slug', $slug)->firstOrFail();
+
+    if (! $post) {abort(404);}
+    
+    return view('posts', ['post' => $post]);
+  }
+}
